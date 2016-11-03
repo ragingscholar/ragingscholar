79225ae1fb71f000df4f5120d5d8c3b672ccae76
Haisenberg <hailang@outlook.com>
2016-11-03 14:02:58 +0800

    restructuring

---
+++ dist/assets/js/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var _dog = require('./dog');
+
+var _dog2 = _interopRequireDefault(_dog);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var str = 'ES6'; /* eslint-disable no-console */
+
+console.log('Hello from ' + str);
+
+var toby = new _dog2.default('Toby');
+console.log(toby.bark());
