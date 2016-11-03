2c17658430ad6da254ba3237d7c120bd70af5f4d
Haisenberg <hailang@outlook.com>
2016-11-03 15:16:43 +0800

    added webpack to stack

---
+++ webpack.config.babel.js
@@ -0,0 +1,19 @@
+/* eslint-disable indent */
+export default {
+    output: {
+        filename: 'client-bundle.js',
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: [/node_modules/],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+    },
+};
