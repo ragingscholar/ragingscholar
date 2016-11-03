7c51e5b86412c560ddd77788e1280c6a48228db4
Haisenberg <hailang@outlook.com>
2016-11-03 10:38:13 +0800

    rewrote gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,38 +5,37 @@ const del = require('del');
 const exec = require('child_process').exec;
 
 const paths = {
-    allSrcJs: 'src/**/*.js',
-    libDir: 'lib',
+    allJsFiles: '_js/**/*.js',
+    jsDir: 'public/assets/js',
     allLessFiles: '_less/*.less',
     cssDir: 'public/assets/css',
 };
 
-gulp.task('clean', () => {
-    return del(paths.libDir);
+gulp.task('clean-css', () => {
+    return del(paths.cssDir);
 });
 
-gulp.task('build', ['clean'], () => {
-    return gulp.src(paths.allSrcJs)
+gulp.task('clean-js', ['clean-css'], () => {
+    return del(paths.jsDir);
+});
+
+gulp.task('build-less', ['clean-js'], () => {
+    return gulp.src(paths.allLessFiles)
+        .pipe(less())
+        .pipe(gulp.dest(paths.cssDir));
+});
+
+gulp.task('build-js', ['build-less'], () => {
+    return gulp.src(paths.allJsFiles)
         .pipe(babel())
-        .pipe(gulp.dest(paths.libDir));
+        .pipe(gulp.dest(paths.jsDir));
 });
 
-gulp.task('main', ['build'], (callback) => {
-    exec(`node ${paths.libDir}`, (error, stdout) => {
+gulp.task('build', ['build-js'], (callback) => {
+    exec(`node ${paths.jsDir}`, (error, stdout) => {
         console.log(stdout);
         return callback(error);
     });
 });
 
-gulp.task('watch', () => {
-    gulp.watch(paths.allSrcJs, ['main']);
-});
-
-gulp.task('default', ['watch', 'main']);
-
-
-gulp.task('build-less', function(){
-    return gulp.src(paths.allLessFiles)
-        .pipe(less())
-        .pipe(gulp.dest(paths.cssDir));
-});
+gulp.task('default', ['build'])
